158fcfbd25e848cbfbe00ab2b3c3118db1916160
Alexis Zugasti <alexiszugasti@gmail.com>
2024-03-11 11:46:20 -0600

    add testing for broken images page

---
+++ cypress/e2e/BorkenImages_Suite/BrokenImages.spec.cy.ts
@@ -0,0 +1,33 @@
+import BrokenImages from "../../pages/BrokenImages";
+
+describe("Test for Broken Images", () => {
+  beforeEach(() => {
+    cy.visit("/broken_images");
+  });
+
+  it("Check for broken images", () => {
+    //Check Title.
+    BrokenImages.elements.title().should("have.text", "Broken Images");
+
+    const imageInfoArray = [];
+
+    const brokenImages = []; //Array for storing all broken images info.
+
+    //Get all images. And test all of them, afterwards, add the broken ones to the array.
+    cy.get(".example img")
+      .each(($element, i) => {
+        const width = $element.prop("naturalWidth");
+        if (width === 0) {
+          brokenImages.push(`Image${i + 1} is broken`);
+        }
+      })
+      .then(() => {
+        if (brokenImages.length) {
+          throw new Error(
+            `${brokenImages.length} IMAGES ARE BROKEN: ` +
+              brokenImages.join(", ")
+          );
+        }
+      });
+  });
+});
